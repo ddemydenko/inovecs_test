e3b3647eb532fc6babb7454c7830185e3a1e39ae
ddemydenko <ddemydenko@luxoft.com>
2018-09-12 01:02:21 +0300

    add helpers to achieve fast test

--- test/helpers.js
+++ test/helpers.js
@@ -1,10 +1,18 @@
-const { User } = require('../models');
+const { User, Deal, Message, sequelize } = require('../models');
 
 module.exports = {
   destroyUsers() {
     return User.destroy({
-      truncate: true, cascade: true,
-      where: { email: 'bruce_wayne@example.com' }
+      truncate: true, cascade: true
+    }).then(() => {
+      return sequelize.query('ALTER SEQUENCE users_id_seq RESTART WITH 1')
+    });
+  },
+  destroyDeals() {
+    return Deal.destroy({
+      truncate: true, cascade: true
+    }).then(() => {
+      return sequelize.query('ALTER SEQUENCE deals_id_seq RESTART WITH 1')
     });
   },
   createUsers() {
@@ -22,7 +30,57 @@ module.exports = {
         lastName: 'Kent',
         email: 'clark.kent@example.com',
         password: '$2b$10$H54IcGd9tp/.B8mCdZc83O9V8rQctjK2u5k4TTDufGWigNf.86rbu'
+      },
+      {
+        id: 3,
+        firstName: 'Peter',
+        lastName: 'Parker',
+        email: 'peter.parker@example.com',
+        password: '$2b$10$H54IcGd9tp/.B8mCdZc83O9V8rQctjK2u5k4TTDufGWigNf.86rbu'
       }
     ]);
+  },
+  createDeals() {
+    return Deal.bulkCreate([
+      {
+        id: 1,
+        authorId: 1,
+        theme: "CD Reader",
+        status: "Open",
+        message: "Hi! I have a CD reader for you at discounted price",
+        amount: 5.5,
+        receiverId: 2,
+        replyTo: 2
+      },
+      {
+        id: 2,
+        authorId: 3,
+        theme: "CD Reader",
+        status: "Open",
+        message: "Hi! I have a CD reader for you at discounted price",
+        amount: 5.5,
+        receiverId: 2,
+        replyTo: 2
+      }
+    ]).then(() => {
+      return Message.bulkCreate([
+        {
+          message: 'Hi! I have a CD reader for you at discounted price',
+          authorId: 1,
+          amount: 5.5,
+          dealId:1
+      },
+        {
+          message: 'Hi! I have a CD reader for you at discounted price',
+          authorId: 3,
+          amount: 5.5,
+          dealId:2
+        }
+      ]);
+    });
   }
+
+
+
+
 }
