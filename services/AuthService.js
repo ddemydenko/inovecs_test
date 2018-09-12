8ed009fa34343e52a7acb0c3a8795c0934ecfb45
ddemydenko <ddemydenko@luxoft.com>
2018-09-12 23:24:52 +0300

    refactoring AuthService with async\await approach

--- services/AuthService.js
+++ services/AuthService.js
@@ -13,61 +13,56 @@ const TokenService = require('../services/TokenService');
 // it must be wrapper on third party auth service, i.e. cognito etc.
 class AuthService {
   // Login
-  signin(req) {
+  async signin(req) {
     const { data } = req.body;
-    let userData;
     isEmail(data.email);
 
-    return User.findOne({
+    const user = await User.findOne({
       attributes: ['id', 'password', 'email', 'firstName'],
       where: { email: data.email },
-    })
-      .then((user) => {
-        if (!user) {
-          throw new CustomError(`User with email: ${data.email} is not enrolled`, 404);
-        } else {
-          userData = {
-            id: user.id,
-            email: user.email,
-            firstName: user.firstName
-          };
-          return cryptCompare(data.password, user.password);
-        }
-      })
-      .then((passwordMatch) => {
-        if (passwordMatch) {
-          return TokenService.encodeToken(userData).then(token => ({ token }));
-        }
-        throw new CustomError('Unauthorized Access. Password does not match', 401);
-      });
+    });
+
+    if (!user) {
+      throw new CustomError(`User with email: ${data.email} is not enrolled`, 404);
+    }
+    const userData = {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName
+    };
+    const passwordMatch = await cryptCompare(data.password, user.password);
+
+    if (passwordMatch) {
+      return TokenService.encodeToken(userData).then(token => ({ token }));
+    }
+    throw new CustomError('Unauthorized Access. Password does not match', 401);
   }
 
-  signup(req) {
+  async signup(req) {
     const {
       firstName, email, password, lastName
     } = req.body.data;
 
     isEmail(email);
 
-    return User.findOne({
+    const findUser = await User.findOne({
       attributes: ['id', 'firstName', 'lastName', 'email'],
       where: { email },
-    })
-      .then((user) => {
-        if (!user) {
-          return cryptHash(password, 10);
-        }
-        throw new CustomError(`User with email: ${email} already enrolled`, 409);
-      })
-      .then((hash) => {
-        return User.create({
-          firstName,
-          lastName,
-          email,
-          password: hash
-        });
-      })
-      .then(user => ({ registrationId: user.id }));
+    });
+
+    if (findUser) {
+      throw new CustomError(`User with email: ${email} already enrolled`, 409);
+    }
+    const hash = await cryptHash(password, 10);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hash
+    });
+
+    return { registrationId: user.id };
   }
 }
 
